de8e103872b771776818251acbd46e049ee0d9fb
Trevorspires <trevor.spires1@gmail.com>
2023-12-01 11:27:59 -0500

    Initial commit

---
+++ Streamlit_App/main.py
@@ -0,0 +1,36 @@
+import InvokeAgent as agenthelper
+import streamlit as st
+
+#to run --> python3 -m streamlit run main.py                                            
+
+st.title("AnyCompany Support Agent :robot_face:")
+
+prompt = st.sidebar.text_area(label="What is your question?", max_chars=2000)
+
+event = {
+  "sessionId": "trevx",
+  "question": prompt
+}
+
+endevent = {
+  "sessionId": "trevx",
+  "question": "placeholder to end session",
+  "endSession": True
+}
+
+endsessionprompt = st.sidebar.button("END SESSION", key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
+
+if prompt:
+    response = agenthelper.lambda_handler(event, None)
+    st.write("Support AI: ",response['body'])
+
+if endsessionprompt:
+    st.write("Thank you for using AnyCompany Support Agent!")
+    response = agenthelper.lambda_handler(endevent, None)
+
+    
+###Example Prompts
+#what are some flux3000 features?
+#I enrich profiles with third-party data?
+#What does Guaranteed Connect do?
+#Can you create a feature request named "Support AWS SNS Integration for Guaranteed Connect" from the company named AnyCompanyCustomer14
---
+++ WorkingLambda.py
@@ -0,0 +1,128 @@
+import json
+import random
+import boto3
+
+#dynamo client
+dynamodb = boto3.client('dynamodb')
+tableName = 'flux3000-feature-requests'
+
+#sns client
+sns = boto3.client('sns')
+topicarn = 'arn:aws:sns:us-west-2:161615149547:FeatureRequests'
+
+def get_named_parameter(event, name):
+
+    return next(item for item in event['parameters'] if item['name'] == name)['value']
+
+def get_named_property(event, name):
+    return next(item for item in event['requestBody']['content']['application/json']['properties'] if item['name'] == name)['value']
+
+def createFeatureRequest(event):
+    print("calling method: create feature request")
+    featureRequestName = get_named_parameter(event, 'featureRequestName')
+    featureRequestDescription = get_named_parameter(event, 'featureRequestDescription') 
+    customerName = get_named_parameter(event, 'customerName') 
+    
+    print (event)
+    
+    # TODO: implement creating featureRequest
+    featureRequestID = str(random.randint(1, 99999))
+
+    
+    item = {
+        'featureRequestID': {"S": featureRequestID},
+        'featureRequestName': {"S": featureRequestName},
+        'featureRequestDescription': {"S": featureRequestDescription},
+        'customerName': {"S": customerName}
+        }
+
+    dynamodb.put_item(
+        TableName=tableName,
+        Item=item
+        )
+    
+    response = sns.publish(
+        TopicArn=topicarn,
+        Message=f"your feature request {featureRequestName} has been created and assigned to trevx@amazon.com",
+        Subject="Feature Request Successfully Created"
+    )
+    
+    print(response['MessageId']) 
+
+
+    return {
+        'body': f"Created request {featureRequestID} in {tableName}"
+    }
+
+def updateFeatureRequest(event):
+    print(event)
+    
+    featureRequestID = str(get_named_parameter(event, 'featureRequestID'))
+    customerName = get_named_parameter(event, 'customerName')
+    
+
+    # TODO: impliment delete from dynamo here?
+    key = {
+        'featureRequestID': {"S": featureRequestID},
+        }
+
+    attribute_updates = {
+        'customerName': {'Value': {'S': customerName}}
+    }
+
+    dynamodb.update_item(
+        TableName=tableName,
+        Key=key,
+        AttributeUpdates=attribute_updates
+    )
+
+    response = sns.publish(
+        TopicArn=topicarn,
+        Message=f"your feature request {featureRequestID} has been updated by trevx@amazon.com",
+        Subject="Feature Request Successfully Updated"
+    )
+
+    print(response['MessageId']) 
+
+    # Return a success message
+    return { 
+        'body': f"Updated request {featureRequestID} in {tableName}"
+    }
+
+    
+def lambda_handler(event, context):
+
+    result = ''
+    response_code = 200
+    action_group = event['actionGroup']
+    api_path = event['apiPath']
+    
+    print ("lambda_handler == > api_path: ",api_path)
+    
+    if api_path == '/createFeatureRequest':
+        result = createFeatureRequest(event)
+    elif api_path == '/updateFeatureRequest':
+        result = updateFeatureRequest(event)
+    else:
+        response_code = 404
+        result = f"Unrecognized api path: {action_group}::{api_path}"
+
+    response_body = {
+        'application/json': {
+            'body': json.dumps(result)
+        }
+    }
+    
+    print ("Event:", event)
+    action_response = {
+        'actionGroup': event['actionGroup'],
+        'apiPath': event['apiPath'],
+        # 'httpMethod': event['HTTPMETHOD'], 
+        'httpMethod': event['httpMethod'], 
+        'httpStatusCode': response_code,
+        'responseBody': response_body
+    }
+
+    api_response = {'messageVersion': '1.0', 'response': action_response}
+        
+    return api_response
